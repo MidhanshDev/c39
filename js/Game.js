de05d7497f98d5f4895c2b9376c549cd9815e421
MidhanshDev <midhanshdevthirumurugan@gmail.com>
2020-09-08 17:33:35 +0530

    reset and created marker

--- js/Game.js
+++ js/Game.js
@@ -70,6 +70,9 @@ class Game {
         cars[index-1].y = y;
 
         if (index === player.index){
+          stroke(10);
+          fill("orange");
+          ellipse(x,y,70,70);
           cars[index - 1].shapeColor = "red";
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y;
